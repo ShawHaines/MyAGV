491ffba66ba7cfeaa7cb579e7af41da2d65fe00a
ShawHaines <haohaistone@qq.com>
2020-04-11 15:52:03 +0800

    Updated .gitignore

--- build/course_agv_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shaw/catkin_ws/src/course_agv_control
-# Build directory: /home/shaw/catkin_ws/build/course_agv_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
