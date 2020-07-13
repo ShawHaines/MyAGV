17e8e1664ae8f8dfeaccb449f667fd3102faf62e
ShawHaines <haohaistone@qq.com>
2020-07-13 16:34:50 +0800

    Cherry-picked centroid extraction and made minor improvements.

--- src/course_agv_slam/scripts/extraction.py
+++ src/course_agv_slam/scripts/extraction.py
@@ -17,25 +17,26 @@ class Extraction():
         labels = []
         # I don't know what trust are, just ignoring them...
         ranges=np.array(msg.ranges)
-        raySize=len(msg.ranges)
-        theta =np.linspace(msg.angle_min,msg.angle_max,raySize)
+        length=np.size(ranges)
+        theta =np.linspace(msg.angle_min,msg.angle_max,length)
         pointCloud=np.vstack((np.multiply(np.cos(theta),ranges),np.multiply(np.sin(theta),ranges)))
         
         # add a index[0] at the end so that diff won't subtract the length by 1
         pointCloud=np.hstack((pointCloud,pointCloud[:,0].reshape(2,1)))
         delta=np.linalg.norm(np.diff(pointCloud),axis=0)
         # jump is between jumpPos and jumpPos+1
-        jumpPos,=np.nonzero(np.abs(delta)>self.range_threshold)
-
-        np.append(jumpPos,jumpPos[0]+raySize)
-        for i in range(np.size(jumpPos)-1):
-            points=jumpPos[i+1]-jumpPos[i]
+        jumpPos,=np.nonzero(np.abs(delta)>self.range_threshold) # because return value is a tuple.
+        for i in range(np.size(jumpPos)):
+            if i<np.size(jumpPos)-1:
+                points=jumpPos[i+1]-jumpPos[i]
+            else:
+                points=jumpPos[0]+length-jumpPos[i]
             # ensure enough points.
             if points>=self.landMark_min_pt:
                 # ensure not too large radius. A rough estimation, radius*angle
-                if ranges[jumpPos[i+1]%raySize]*msg.angle_increment*points<=self.radius_max_th*2:
-                    # This is a landmark, use the average index
-                    labels.append(((jumpPos[i]+1+jumpPos[i+1])//2)%raySize)
+                if ranges[(jumpPos[i]+1)%length]*msg.angle_increment*points<=self.radius_max_th*2:
+                    # store a tuple into labels.
+                    labels.append((jumpPos[i],jumpPos[i+1 if i+1<np.size(jumpPos) else 0]))
         return self.extractLandMark(msg,labels,trust)
         
     #  What's the difference between the two functions?
@@ -44,11 +45,15 @@ class Extraction():
         no longer use landMarkSet class. Use pointCloud (2*n array) as return value.
         '''
         ranges=np.array(msg.ranges)
-        theta =np.linspace(msg.angle_min,msg.angle_max,len(msg.ranges))
+        total_num=len(msg.ranges)
+        angle_l = np.linspace(msg.angle_min,msg.angle_max,total_num)
+        pc = np.vstack((np.multiply(np.cos(angle_l),ranges),np.multiply(np.sin(angle_l),ranges)))
 
-        ranges=ranges[labels]
-        theta =theta[labels]
-        # id is not important...
-        # landmark.id=labels
-        landmark=np.vstack((np.multiply(np.cos(theta),ranges),np.multiply(np.sin(theta),ranges)))
+        length=len(labels)
+        landmark=np.zeros((2,length))
+        for index,(i,j) in enumerate(labels):
+            if i<j:
+                landmark[:,index]=np.mean(pc[:,np.arange(i+1,j+1)],axis=1)
+            else:
+                landmark[:,index]=np.mean(pc[:,np.hstack((np.arange(i+1,total_num),np.arange(j+1)))],axis=1)
         return landmark
